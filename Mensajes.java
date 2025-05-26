c2112f34a69b03168dc751a3505177dc58eb3cdf
alvaropas17 <alvpastor17@gmail.com>
2025-05-26 10:32:39 +0200

    Javadoc añadido
    Se han añadido los javadoc

---
+++ Mensajes.java
@@ -0,0 +1,4 @@
+public class Mensajes {
+    public static final String NO_HAY_EMPLEADOS = "No hay empleados registrados.";
+    public static final String LISTA_EMPLEADOS = "Lista de Empleados:";
+}
