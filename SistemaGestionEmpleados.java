a3c3b8e3011075ae6ab8c6ca2d34186a0ef45df0
Azabache23 <gzubache@gmail.com>
2025-05-26 09:21:03 +0200

    Modificación de los atributos de empleado y generación de getters y setters.

--- SistemaGestionEmpleados.java
+++ SistemaGestionEmpleados.java
@@ -1,17 +1,19 @@
 import java.util.Scanner;
 
-public class SistemaGestionEmpleados {
-
+public class SistemaGestionEmpleados 
+{
     private Empleado[] empleados;
 
-    public SistemaGestionEmpleados(Empleado[] empleados) {
+    public SistemaGestionEmpleados(Empleado[] empleados) 
+    {
         this.empleados = empleados;
     }
 
-    public void aumentarSalario(double porcentaje) {
+    public void aumentarSalario(double porcentaje) 
+    {
         for (Empleado empleado : empleados) {
-            double nuevoSalario = empleado.salario * (1 + porcentaje / 100);
-            empleado.salario = nuevoSalario;
+            double nuevoSalario = empleado.getSalario() * (1 + porcentaje / 100);
+            empleado.setSalario(nuevoSalario);
         }
     }
 
