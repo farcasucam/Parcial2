195c13aaff775d2c7765b7a9a22e46e1ed868b56
jibuendia <jibuendia@alu.ucam.edu>
2025-05-26 09:44:56 +0200

    Centralizados los textos fijos en la clase Mensajes para facilitar traducción y mantenimiento

---
+++ Mensajes.java
@@ -0,0 +1,4 @@
+public class Mensajes {
+    public static final String INTRODUCE_PORCENTAJE = "Introduzca el porcentaje de aumento de salario: ";
+    public static final String LISTA_EMPLEADOS = "Lista de Empleados:";
+}
